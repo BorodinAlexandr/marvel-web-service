816b7fdf79c76f2d1637ebd8830368ac474e752e
Borodin Alexandr <bvachtang@gmail.com>
2022-08-18 22:36:11 +0300

    добавить поиск по имени персонажа и переход на страницу с персонажем

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -9,25 +9,30 @@ const useMarvelService = () => {
 
   const getAllCharacters = async (offset = _baseOffset) => {
     const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
-    return res.data.results.map(transformCharacter);
+    return res.data.results.map(_transformCharacter);
+  };
+
+  const getCharacterByName = async (name) => {
+    const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+    return res.data.results.map(_transformCharacter);
   };
 
   const getCharacter = async (id) => {
     const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
-    return transformCharacter(res.data.results[0]);
+    return _transformCharacter(res.data.results[0]);
   };
 
   const getAllComics = async (offset = 0) => {
     const res = await request(`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`);
-    return res.data.results.map(transformComics);
+    return res.data.results.map(_transformComics);
   };
 
   const getComic = async (id) => {
     const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
-    return transformComics(res.data.results[0]);
+    return _transformComics(res.data.results[0]);
   };
 
-  const transformCharacter = (char) => {
+  const _transformCharacter = (char) => {
     return {
       id: char.id,
       name: char.name,
@@ -41,7 +46,7 @@ const useMarvelService = () => {
     };
   };
 
-  const transformComics = (comics) => {
+  const _transformComics = (comics) => {
     return {
       id: comics.id,
       title: comics.title,
@@ -49,11 +54,11 @@ const useMarvelService = () => {
       pageCount: comics.pageCount ? `${comics.pageCount} p.` : 'No information about the number of pages',
       thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
       language: comics.textObjects.language || 'en-us',
-      price: comics.prices.price ? `${comics.prices.price}$` : 'not available',
+      price: comics.prices[0].price ? `${comics.prices[0].price}$` : 'not available',
     };
   };
 
-  return { loading, error, clearError, getAllCharacters, getCharacter, getAllComics, getComic };
+  return { loading, error, clearError, getAllCharacters, getCharacterByName, getCharacter, getAllComics, getComic };
 };
 
 export default useMarvelService;
